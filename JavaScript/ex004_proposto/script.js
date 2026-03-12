220096db0871516e5a7aecb720d292da87b36c04
Fernando955073 <fasx.adm@gmail.com>
2026-03-12 16:48:37 -0300

    ex004 final js

--- JavaScript/ex004_proposto/script.js
+++ JavaScript/ex004_proposto/script.js
@@ -1,4 +1,8 @@
 window.confirm ('Benvindo ao meu Site');
-function calc(){
-    var num = window.prompt ("digite um número")
+
+var resultado = document.getElementById ('resultado');
+
+function calc() {
+    var num = Number(window.prompt ("digite um número"))
+    resultado.innerHTML = `O dobro do valor é ${num*2} e a matade é ${num/2}.`
 }
